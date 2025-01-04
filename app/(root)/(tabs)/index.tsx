acb68be26d5f753e75dae2b965a8ffaae6b0c5ce
Aman Shahid <shahidamaan2019@gmail.com>
2025-01-04 23:56:21 +0500

    screens: finished profile screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,10 +5,7 @@ import { Link } from 'expo-router'
 export default function Index() {
     return (
         <View className='h-full w-full flex justify-center items-center'>
-            <Text className='my-10 text-3xl font-rubik font-bold text-primary-300'>Root Index of the app</Text>
-            <Link href="/explore">Explore Page</Link>
-            <Link href="/profile">Profile Page</Link>
-            <Link href="/properties/23">properties Page</Link>
+            <Text className='my-10 text-3xl font-rubik font-bold text-primary-300'>Welcome To ReState!</Text>
             <Link href="/sign-in">Sign In Page</Link>
         </View>
     )
