80470695863d8f6adc1679826369e3eb46ba512a
Aman Shahid <shahidamaan2019@gmail.com>
2025-01-05 21:57:28 +0500

    Screens: Started Home Screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
+import Search from '@/components/Search'
+import { Card, FeaturedCard } from '@/components/Cards'
 
 export default function Index() {
     return (
-        <View className='h-full w-full flex justify-center items-center'>
-            <Text className='my-10 text-3xl font-rubik font-bold text-primary-300'>Welcome To ReState!</Text>
-            <Link href="/sign-in">Sign In Page</Link>
-        </View>
+        <SafeAreaView className='bg-white h-full'>
+            <View className='px-5'>
+                <View className='flex flex-row items-center justify-between mt-5'>
+                    <View className='flex flex-row items-center'>
+                        <Image source={images.avatar} className='size-12 rounded-full' />
+                        <View className='flex flex-col items-start ml-2 justify-center'>
+                            <Text className='text-xs font-rubik text-black-100'>Good Morning!</Text>
+                            <Text className='text-base font-rubik-medium text-black-300'>Aman Shahid</Text>
+                        </View>
+                    </View>
+                    <Image source={icons.bell} className='size-6' />
+                </View>
+                <Search />
+                <View className='my-5'>
+                    <View className='flex flex-row items-center justify-between'>
+                        <Text className='text-xl font-rubik-bold text-black-300'>Featured</Text>
+                        <TouchableOpacity>
+                            <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <FeaturedCard />
+                <Card />
+            </View>
+        </SafeAreaView>
     )
 }
