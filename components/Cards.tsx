51b36887cb9384c70512d74f5eaa4fb9f5d1e170
dev-sire <shahidamaan2019@gmail.com>
2025-01-11 22:43:14 +0500

    finished home ui

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -30,18 +30,18 @@ export const FeaturedCard = ({ onPress }: Props) => {
 
 export const Card = ({ onPress }: Props) => {
     return(
-        <TouchableOpacity onPress={onPress} className='flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/50 relative'>
-            <View className='flex flex-row items-center absolute top-5 right-5 px-2 bg-white/90 p-1 rounded-full z-50'>
-                <Image source={icons.star} className='size-2.5' />
-                <Text className='text-xs text-primary-300 ml-0.5 font-rubik-bold'>4.4</Text>
+        <TouchableOpacity className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative" onPress={onPress}>
+            <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+                <Image source={icons.star} className="size-2.5" />
+                <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">4.4</Text>
             </View>
-            <Image source={images.newYork} className='w-full h-40 rounded-lg' />
-            <View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
-                <Text className='text-xl font-rubik-extrabold text-white' numberOfLines={1}>Modern Apartment</Text>
-                <Text className='text-base font-rubik text-white'>9-0, Mocha Chowk, Karachi</Text>
-                <View className='flex flex-row items-center justify-between w-full'>
-                    <Text className='text-xl font-rubik-extrabold text-white'>$2500</Text>
-                    <Image source={icons.heart} className='size-5' />
+            <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+            <View className="flex flex-col mt-2">
+                <Text className="text-base font-rubik-bold text-black-300">Luxury Apartment</Text>
+                <Text className="text-xs font-rubik text-black-100">9-0, Mocha Chowk, Karachi</Text>
+                <View className="flex flex-row items-center justify-between mt-2">
+                    <Text className="text-base font-rubik-bold text-primary-300">$2780</Text>
+                    <Image source={icons.heart} className="w-5 h-5 mr-2" tintColor="#191D31" />
                 </View>
             </View>
         </TouchableOpacity>
